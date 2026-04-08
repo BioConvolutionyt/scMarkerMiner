1a25370b625e761fa5c7732309eabf620ac2f83f
YtLiu <YtLiu-53846@outlook.com>
2026-04-08 11:24:08 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,7 @@ DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME", "scmarkerminer")
 DB_CHARSET = "utf8mb4"
+DB_SSL = os.getenv("DB_SSL", "0") == "1"
 
 SQLALCHEMY_DATABASE_URL = (
     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
